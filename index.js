865a3856d510f0229617065fca3a81a711e999e4
Luke Bennett <luke.bennett@bravissimo.com>
2016-08-02 12:38:10 +0100

    Disable react/require-extension rule as it's now deprecated

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ module.exports = {
     'eslint-config-bravissimo',
     'eslint-config-airbnb/rules/react',
     'eslint-config-airbnb/rules/react-a11y'
-  ].map(require.resolve)
+  ].map(require.resolve),
+  rules: {
+    'react/require-extension': 0
+  }
 };
