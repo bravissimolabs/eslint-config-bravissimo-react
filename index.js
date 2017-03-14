db32c33ac6cc7b67426b95200d72aaabe092fb28
Paul Bill <paul.bill@bravissimo.com>
2017-03-14 13:35:11 +0000

    Ignore eslint rules that are not required.

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ module.exports = {
     'eslint-config-airbnb/rules/react-a11y'
   ].map(require.resolve),
   rules: {
-    'react/require-extension': 0
+    'react/require-extension': 0,
+
+    // Disabled due to clashes with react-intl's 'style' prop.
+    'react/style-prop-object': 'off',
+
+    // Disabled to help prevent browser compatibility issues.
+    'jsx-a11y/no-static-element-interactions': 'off'
   }
 };
