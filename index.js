80c2c67397ca4439efbe2f6a541735ec2bd0e349
Paul Bill <me@paulbill.com>
2017-03-16 12:18:59 +0000

    Bump to the latest version of eslint-config-airbnb (#1)
    * Bump to the latest version of eslint-config-airbnb
    
    * Add missing peer dependencies
    
    * Ignore eslint rules that are not required.
    
    * Add flow type dependencies
    
    * Update version numbers in changelog

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ module.exports = {
     'eslint-config-airbnb/rules/react-a11y'
   ].map(require.resolve),
   rules: {
-    'react/require-extension': 0
+    'react/require-extension': 0,
+
+    'react/sort-comp': [2, {
+      'order': [
+        'type-annotations',
+        'static-methods',
+        'lifecycle',
+        'everything-else',
+        '/^render.+$/',
+        'render'
+      ]
+    }],
+
+    // Disabled due to clashes with react-intl's 'style' prop.
+    'react/style-prop-object': 'off',
+
+    // Disabled to help prevent browser compatibility issues.
+    'jsx-a11y/no-static-element-interactions': 'off',
+
+    // Disabled as we use flow type.
+    'react/forbid-prop-types': 'off',
+    'react/require-default-props': 'off'
   }
 };
